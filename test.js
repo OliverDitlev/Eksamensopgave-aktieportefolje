b2a02502a743a6aba00f5ef4ab9897d86e93f836
Jakob Bundgaard <jakobb1234@gmail.com>
2025-03-27 10:29:12 +0100

    Update test.js

--- test.js
+++ test.js
@@ -3,3 +3,25 @@ console.log('tester om app virker')
 console.log('hej')
 console.log("test2")}
 console.log("test3")
+
+
+
+
+let a = [1,2];
+
+function random(a) {
+  let b = Math.floor(Math.random() * a.length)
+  return b;
+};
+
+function hejfarvel() {
+  random(a);
+
+  if (b === 1) {
+    console.log("hej")
+  } else if (b === 2) {
+    console.log("farvel")
+  }
+}
+
+hejfarvel()
