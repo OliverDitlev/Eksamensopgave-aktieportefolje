484d972819c388549acdc81e1e0d5d1fa16e61f1
OliverDitlev <oldi24ab@student.cbs.dk>
2025-03-27 10:40:20 +0100

    package.json

--- test.js
+++ test.js
@@ -1,38 +1,10 @@
 for (i=0;i<2;i++){
 console.log('tester om app virker')
 console.log('hej')
-<<<<<<< HEAD
+
 console.log("test2")
 console.log("test jacob")}
-=======
-console.log("test2")}
+
+console.log("test2")
 console.log("test3")
 
-
-<<<<<<< HEAD
-//jonas version 
-=======
-
-
-let a = [1,2];
-
-function random(a) {
-  let b = Math.floor(Math.random() * a.length)
-  return b;
-};
-
-function hejfarvel() {
-  random(a);
-
-  if (b === 1) {
-    console.log("hej")
-  } else if (b === 2) {
-    console.log("farvel")
-  }
-}
-
-hejfarvel()
-
-kage
->>>>>>> d904bfe67f89b7ce26a6bc77b26637ebc135dff8
->>>>>>> adf141b1bba07eae921172a991e11b4339a4c1de
