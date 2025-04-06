f25b434a874d4f0ba2d6a16e6147d6b8363d5d6c
OliverDitlev <oldi24ab@student.cbs.dk>
2025-04-06 11:17:18 +0200

    menu2

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,7 @@ const port = 3000
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '../views'))
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.get('/',(req, res) => {
     res.render('dashboard');
