3706365d24896a5b71fe3c491c2115d373ae1427
OliverDitlev <oldi24ab@student.cbs.dk>
2025-04-06 10:50:16 +0200

    menu 1

--- Backend/app.js
+++ Backend/app.js
@@ -3,13 +3,11 @@ const path = require('path')
 const app = express()
 const port = 3000
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '../views'))
 
 app.get('/',(req, res) => {
-    res.render('front-page');
+    res.render('dashboard');
 });
 
 app.get('/loginpage', (req,res) => {
