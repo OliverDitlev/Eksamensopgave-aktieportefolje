d9a65cfbac71ccf5f0d326004bd62107f4f47585
Jakob Bundgaard <jakobb1234@gmail.com>
2025-05-05 10:39:46 +0200

    Update accountroutes.js

--- Backend/Routes/accountroutes.js
+++ Backend/Routes/accountroutes.js
@@ -20,15 +20,11 @@ body('password')
 .trim()
 .isLength({ min: 8 }).withMessage('Password must be at least 8 characters long')
 .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter')
-.matches(/[a-z]/).withMessage('Password must contain at least one lowercase letter'),,
+.matches(/[a-z]/).withMessage('Password must contain at least one lowercase letter'),
 
 body('repeatpassword')
 .trim()
-
 .custom((value, { req }) => {
-
-    
-
   if (value !== req.body.password) {
     throw new Error('Passwords do not match');
   }
@@ -41,17 +37,17 @@ body('repeatpassword')
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      // gøre at vi bliver på siden ved fejl
+      // Gør at vi bliver på siden ved fejl
       return res.render('createaccount',{
-        //returnere fejlen til ejs som array så dette kan tilgås
+        // Returnerer fejlen til ejs som array så dette kan tilgås
         errors: errors.array(),
-        //gemmer det gamle input så brugeren ikke skal starte forfra
+        // Gemmer det gamle input så brugeren ikke skal starte forfra
         oldInput: req.body
       })
     }
 
+// Gemmer den nye bruger i databasen
 const { firstname, lastname, email, password } = req.body;
-
 const userId = db.insertUser({
   firstname,
   lastname,
@@ -59,10 +55,12 @@ const userId = db.insertUser({
   password,
 });
 
+// Redirecter til forsiden efter oprettelse
 res.redirect('/');
 });
 
 router.post('/login', [
+// Validerer inputtet til login
   body('email')
   .trim()
   .notEmpty(). withMessage('Email required'),
@@ -73,9 +71,10 @@ router.post('/login', [
 ], async (req, res) =>{
   const errors = validationResult(req);
   const db = req.app.locals.db
-  //bruges til opsamle fejl fra både vallidator og db
+  // Bruges til opsamle fejl fra både vallidator og db
   const errorList = errors.array()
 
+  // Viser loginsiden igen, hvis valideringen giver fejl
   if(!errors.isEmpty()){
       return res.render('login',{
         errors: errors.errorList,
@@ -96,7 +95,7 @@ try{
       oldInput: req.body
     });
   }
-//gemmer brugeren info under sessionen
+// Gemmer brugeren info under sessionen
     req.session.user = 
       {
         user_id: user.user_id,
