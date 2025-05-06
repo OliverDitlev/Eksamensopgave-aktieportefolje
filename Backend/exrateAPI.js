46642e3973de3f2da8d9d540c19f1a6991e480d7
OliverDitlev <oldi24ab@student.cbs.dk>
2025-05-06 21:07:51 +0200

    tt

--- Backend/exrateAPI.js
+++ Backend/exrateAPI.js
@@ -8,18 +8,20 @@ async function getExchangeRates() {
       if (!response.ok) {
         throw new Error(`API request failed: ${response.status}`);
       }
-      console.log(response)
+
       const data = await response.json();
       const rates = data.conversion_rates;
 
-      //array: [dkk,gpb,usd]
-      const dkk = rates['DKK'];
-      const gbp = rates['GBP'];
-      const usd = rates['USD'];
+      const result ={
+        DKK: rates['DKK'],
+        GBP: rates['GBP'],  
+        USD: rates['USD']
+      }
 
-      return [dkk, gbp, usd];
+      return result;
     } catch (error) {
       console.error('could not get exchange rates', error);
       return false;
     }
   }
+  module.exports = { getExchangeRates }
