0bc30740111a7a58354cd81700bd13925b260f9a
OliverDitlev <oldi24ab@student.cbs.dk>
2025-05-08 21:04:18 +0200

    dgf

--- Backend/api.js
+++ Backend/api.js
@@ -2,7 +2,7 @@
 const request = require('request');
 const sql = require('mssql')
 
-const API_KEY = 'P8HUIXTH2ZGN091S'; 
+const API_KEY = '6YAB1UHQAZIKYPX1'; 
 
 function getStockData(companyName , db = null) {
 
